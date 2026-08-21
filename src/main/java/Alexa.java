7bde3a80a64b25e482043f8444a19a3fffdc1f96
Edmund Tan <edmundtan4@gmail.com>
2026-08-21 11:22:53 +0800

    Level-2 Add, List

--- src/main/java/Alexa.java
+++ src/main/java/Alexa.java
@@ -1,5 +1,6 @@
 package src.main.java;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * A chatbot that greets the user before ending the program.
@@ -7,7 +8,9 @@ import java.util.Scanner;
 public class Alexa {
     /** A visual divider used to frame the chatbot's messages. */
     private static final String DIVIDER = "____________________________________________________________";
-
+    private static ArrayList<New_task> tasks = new ArrayList<>();
+    private static int task_counter = 1;
+    record New_task(int num, String t) {}
     /**
      * Starts Alexa, displays its greeting, and exits.
      *
@@ -21,30 +24,38 @@ public class Alexa {
         System.out.println("Hello! I'm Alexa.");
         System.out.println("What can I do for you?");
         System.out.println(DIVIDER);
-//        System.out.println("Bye. Hope to see you again soon!");
-//        System.out.println(DIVIDER);
 
         while (true) {
-//            System.out.println("What can I do for you?");
             String choice = input.nextLine();
+            input_statement(choice);
             if (choice.equals("bye")) {
-                input_statement(choice);
                 break;
-            } else {
-                input_statement(choice);
             }
-
         }
 
 
     }
     static void input_statement(String text) {
+        New_task new_task = new New_task(task_counter++, text);
         if (text.equals("bye")) {
+            System.out.println(DIVIDER);
             System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(DIVIDER);
+        } else if (text.equals("list")) {
+            System.out.println(DIVIDER);
+            print_tasks();
+            System.out.println(DIVIDER);
         } else {
+            tasks.add(new_task);
             System.out.println(DIVIDER);
-            System.out.println(text);
+            System.out.println(new_task.num + ". " +  new_task.t);
             System.out.println(DIVIDER);
+        }
+    }
+
+    static void print_tasks () {
+        for  (New_task task : tasks) {
+            System.out.println(task.num + ". " + task.t);
         }
     }
 }
