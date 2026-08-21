6b1311e7478f8aae7cb12f5b8142602d44078fc8
Edmund Tan <edmundtan4@gmail.com>
2026-08-21 11:00:32 +0800

    Level-1 Echo

--- src/main/java/Alexa.java
+++ src/main/java/Alexa.java
@@ -1,4 +1,5 @@
 package src.main.java;
+import java.util.Scanner;
 
 /**
  * A chatbot that greets the user before ending the program.
@@ -13,12 +14,37 @@ public class Alexa {
      * @param args command-line arguments, which are not used
      */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         System.out.println(DIVIDER);
         System.out.println("                 A L E X A");
         System.out.println("Hello! I'm Alexa.");
         System.out.println("What can I do for you?");
         System.out.println(DIVIDER);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(DIVIDER);
+//        System.out.println("Bye. Hope to see you again soon!");
+//        System.out.println(DIVIDER);
+
+        while (true) {
+//            System.out.println("What can I do for you?");
+            String choice = input.nextLine();
+            if (choice.equals("bye")) {
+                input_statement(choice);
+                break;
+            } else {
+                input_statement(choice);
+            }
+
+        }
+
+
+    }
+    static void input_statement(String text) {
+        if (text.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(DIVIDER);
+            System.out.println(text);
+            System.out.println(DIVIDER);
+        }
     }
 }
